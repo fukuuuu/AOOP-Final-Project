68e15662339254e097f7122730920978170ab6b1
Fuku <90387760+fukuuuu@users.noreply.github.com>
2022-11-15 23:22:57 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,6 +3,6 @@ package com.mycompany.test1; //change your own package
 public class Test1 {
 
     public static void main(String[] args) {
-        System.out.println("Hi catssss");
+        System.out.println("Hello world");
     }
 }
